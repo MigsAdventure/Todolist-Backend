245490b23d3cf19398f7afcf6c649eb898201fb4
Miguel Pardo <migsub77@gmail.com>
2016-10-10 11:18:09 -0700

    todolist backend

---
+++ app.js
@@ -0,0 +1,70 @@
+const PORT = 8000;
+const express = require('express');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+
+const Task = require('./models/Task');
+
+const app = express();
+
+app.use(morgan('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+
+//ROUTES
+
+// app.get('/todos', (req, res) => {
+//   Task.getAll((err, tasks) => {
+//     if (err) return res.status(400).send(err);
+//     res.send(tasks);
+//   })
+// });
+
+app.get('/todos?', (req,res) => {
+  
+  Task.filterIsComplete(req, (err,task) => {
+    if (err) return res.status(400).send(err);   
+    res.send(task);
+
+  });
+  // console.log('category: ', categories )
+})
+
+
+app.post('/todos', (req,res) => {
+  Task.create(req.body, err => {
+    if (err) return res.status(400).send(err);
+    res.send(`new task was added: \n ${JSON.stringify(req.body)}`);
+  })
+});
+
+
+app.delete('/todos/:id', (req, res) => {
+  Task.delete(req,err => {
+  if(err) return res.status(400).send(err);
+ });
+ res.send('deleted task');
+}) 
+
+app.delete('/todos/complete', (req, res) => {
+  Task.delete(req, err => {
+  if(err) return res.status(400).send(err);
+ });
+ res.send('deleted task');
+}) 
+
+
+app.put('/todos/:id', (req,res) => {
+  Task.update(req, (err, updatedTask) => {
+    if(err) return res.status(400).send(err);
+    res.send(updatedTask)
+  })
+  
+})
+
+
+app.listen(PORT, err => {
+  console.log(err || `Express listening on port ${PORT}`);
+})
+
---
+++ models/Task.js
@@ -0,0 +1,94 @@
+const fs =require('fs');
+const path = require('path');
+const uuid = require('uuid');
+const _  = require('lodash');
+const moment = require('moment');
+
+const filename = path.join(__dirname, '../data/tasks.json');
+
+exports.getAll = function(cb) {
+  fs.readFile(filename, (err, buffer)  => {
+    if (err) return cb(err);
+    try {
+      var data = JSON.parse(buffer);
+    } catch(e) {
+      var data = [];
+    }
+
+    cb(null, data);
+  })
+}
+
+exports.write = function(newData, cb) {
+  let json = JSON.stringify(newData);
+  fs.writeFile(filename, json, cb);
+}
+
+
+exports.create = function(newTask, cb) {
+  exports.getAll((err, tasks) => {
+    if (err) return cb(err);
+    newTask.id = uuid();
+    newTask.isComplete = 'false';
+    tasks.push(newTask);
+    exports.write(tasks, cb);
+  });
+}
+
+exports.delete = function(req, cb){
+  exports.getAll((err, tasks) => {
+    let deleteTask = req.params.id;
+    let newtasks = tasks.filter(task => {
+       let state = task.isComplete;
+      if (deleteTask === "complete") {
+        if (state !== "true")
+        return task
+      } else if (deleteTask !== task.id) {
+        return task;
+      }
+    })
+    console.log('newTasks:', newtasks)
+    exports.write(newtasks, cb)
+  })
+}
+
+
+exports.update = function(req, cb) {
+  exports.getAll((err, tasks) => {
+    let taskId = req.params.id;
+    let updatedtasks = tasks.map(task => {
+      if(taskId === task.id) {
+        task.isComplete === false ? task.isComplete = true : task.isComplete = false
+      }
+      return task
+    })
+    exports.write(updatedtasks, cb);
+
+    cb(null, updatedtasks)
+  })
+}
+
+exports.filterIsComplete = function(req, cb) {
+  exports.getAll((err, tasks)  => {
+    if(err) return cb(err);
+    let booleanState = req.query.complete;
+    console.log('boolstate:',booleanState)
+    let completeState = tasks.filter(task => {
+        let state = task.isComplete;
+          if (state === booleanState) {
+          return task
+      }
+    })
+    
+        cb(null, completeState)
+ 
+  })// end of getALl
+}
+
+
+
+
+
+
+
+
